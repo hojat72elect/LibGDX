6166d30a2ee8cad910d827c6d277c539351d58ea
hojat72elect <hojat72elect@gmail.com>
2025-12-12 02:42:31 -0500

    ICodeProgress.java -> kt
    #7

--- gdx/src/com/badlogic/gdx/utils/compression/ICodeProgress.java
+++
@@ -1,5 +0,0 @@
-package com.badlogic.gdx.utils.compression;
-
-public interface ICodeProgress {
-    void SetProgress(long inSize, long outSize);
-}
---
+++ gdx/src/com/badlogic/gdx/utils/compression/ICodeProgress.kt
@@ -0,0 +1,5 @@
+package com.badlogic.gdx.utils.compression
+
+interface ICodeProgress {
+    fun SetProgress(inSize: Long, outSize: Long)
+}
