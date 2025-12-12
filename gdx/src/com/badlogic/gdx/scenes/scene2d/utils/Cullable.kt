e683752e9bd997068fb434afecbb55d748011482
hojat72elect <hojat72elect@gmail.com>
2025-12-12 00:01:05 -0500

    Cullable.java -> kt
    #7

--- gdx/src/com/badlogic/gdx/scenes/scene2d/utils/Cullable.kt
+++ gdx/src/com/badlogic/gdx/scenes/scene2d/utils/Cullable.kt
@@ -1,16 +1,16 @@
-package com.badlogic.gdx.scenes.scene2d.utils;
+package com.badlogic.gdx.scenes.scene2d.utils
 
-import com.badlogic.gdx.math.Rectangle;
-import com.badlogic.gdx.utils.Null;
+import com.badlogic.gdx.math.Rectangle
+import com.badlogic.gdx.utils.Null
 
 /**
  * Allows a parent to set the area that is visible on a child actor to allow the child to cull when drawing itself. This must
  * only be used for actors that are not rotated or scaled.
- *
- *  */
-public interface Cullable {
+ */
+interface Cullable {
+
     /**
      * @param cullingArea The culling area in the child actor's coordinates.
      */
-    void setCullingArea(@Null Rectangle cullingArea);
+    fun setCullingArea(@Null cullingArea: Rectangle?)
 }
