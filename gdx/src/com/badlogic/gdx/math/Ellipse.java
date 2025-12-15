158e5b6ab0ff13e15df1a4371a25efd1d12acef7
hojat72elect <hojat72elect@gmail.com>
2025-12-15 00:01:21 -0500

    docs

--- gdx/src/com/badlogic/gdx/math/Ellipse.java
+++ gdx/src/com/badlogic/gdx/math/Ellipse.java
@@ -4,7 +4,8 @@ import com.badlogic.gdx.utils.NumberUtils;
 import java.io.Serializable;
 
 /**
- * A convenient 2D ellipse class, based on the circle class
+ * A convenient 2D ellipse class, based on the circle class.
+ * بیضی
  */
 public class Ellipse implements Serializable, Shape2D {
 
@@ -13,15 +14,14 @@ public class Ellipse implements Serializable, Shape2D {
     public float width, height;
 
     /**
-     * Construct a new ellipse with all values set to zero
+     * Construct a new ellipse with all values set to zero.
      */
     public Ellipse() {
 
     }
 
     /**
-     * Copy constructor
-     *
+     * Copy constructor (copies characteristics of an ellipse into another ellipse).
      * @param ellipse Ellipse to construct a copy of.
      */
     public Ellipse(Ellipse ellipse) {
@@ -32,12 +32,10 @@ public class Ellipse implements Serializable, Shape2D {
     }
 
     /**
-     * Constructs a new ellipse
-     *
-     * @param x      X coordinate of the center of the ellipse
-     * @param y      Y coordinate of the center of the ellipse
-     * @param width  the width of the ellipse
-     * @param height the height of the ellipse
+     * @param x      X coordinate of the center of the ellipse.
+     * @param y      Y coordinate of the center of the ellipse.
+     * @param width  the width of the ellipse.
+     * @param height the height of the ellipse.
      */
     public Ellipse(float x, float y, float width, float height) {
         this.x = x;
@@ -47,11 +45,9 @@ public class Ellipse implements Serializable, Shape2D {
     }
 
     /**
-     * Constructs a new ellipse
-     *
-     * @param position Position vector of the center of the ellipse
-     * @param width    the width of the ellipse
-     * @param height   the height of the ellipse
+     * @param position Position vector of the center of the ellipse.
+     * @param width    the width of the ellipse.
+     * @param height   the height of the ellipse.
      */
     public Ellipse(Vector2 position, float width, float height) {
         this.x = position.x;
@@ -61,10 +57,8 @@ public class Ellipse implements Serializable, Shape2D {
     }
 
     /**
-     * Constructs a new ellipse
-     *
-     * @param position Position vector of the center of the ellipse
-     * @param size     Size vector
+     * @param position Position vector of the center of the ellipse.
+     * @param size Size vector.
      */
     public Ellipse(Vector2 position, Vector2 size) {
         this.x = position.x;
@@ -74,21 +68,18 @@ public class Ellipse implements Serializable, Shape2D {
     }
 
     /**
-     * Constructs a new {@link Ellipse} from the position and radius of a {@link Circle} (since circles are special cases of
-     * ellipses).
-     *
-     * @param circle The circle to take the values of
+     * Constructs a new {@link Ellipse} from the position and radius of a {@link Circle} (since circles are special cases of ellipses).
+     * @param circle The circle to take the values of.
      */
     public Ellipse(Circle circle) {
         this.x = circle.x;
         this.y = circle.y;
-        this.width = circle.radius * 2f;
-        this.height = circle.radius * 2f;
+        this.width = circle.radius * 2F;
+        this.height = circle.radius * 2F;
     }
 
     /**
-     * Checks whether or not this ellipse contains the given point.
-     *
+     * Checks whether this ellipse contains the given point.
      * @param x X coordinate
      * @param y Y coordinate
      * @return true if this ellipse contains the given point; false otherwise.
@@ -97,13 +88,12 @@ public class Ellipse implements Serializable, Shape2D {
         x = x - this.x;
         y = y - this.y;
 
-        return (x * x) / (width * 0.5f * width * 0.5f) + (y * y) / (height * 0.5f * height * 0.5f) <= 1.0f;
+        return (x * x) / (width * 0.5F * width * 0.5F) + (y * y) / (height * 0.5F * height * 0.5F) <= 1.0F;
     }
 
     /**
-     * Checks whether or not this ellipse contains the given point.
-     *
-     * @param point Position vector
+     * Checks whether this ellipse contains the given point.
+     * @param point Position vector.
      * @return true if this ellipse contains the given point; false otherwise.
      */
     public boolean contains(Vector2 point) {
@@ -112,11 +102,10 @@ public class Ellipse implements Serializable, Shape2D {
 
     /**
      * Sets a new position and size for this ellipse.
-     *
-     * @param x      X coordinate of the center of the ellipse
-     * @param y      Y coordinate of the center of the ellipse
-     * @param width  the width of the ellipse
-     * @param height the height of the ellipse
+     * @param x      X coordinate of the center of the ellipse.
+     * @param y      Y coordinate of the center of the ellipse.
+     * @param width  the width of the ellipse.
+     * @param height the height of the ellipse.
      */
     public void set(float x, float y, float width, float height) {
         this.x = x;
@@ -127,7 +116,6 @@ public class Ellipse implements Serializable, Shape2D {
 
     /**
      * Sets a new position and size for this ellipse based upon another ellipse.
-     *
      * @param ellipse The ellipse to copy the position and size of.
      */
     public void set(Ellipse ellipse) {
@@ -137,11 +125,14 @@ public class Ellipse implements Serializable, Shape2D {
         height = ellipse.height;
     }
 
+    /**
+     * If I'm not mistaken, this function converts our ellipse to a circle.
+     */
     public void set(Circle circle) {
         this.x = circle.x;
         this.y = circle.y;
-        this.width = circle.radius * 2f;
-        this.height = circle.radius * 2f;
+        this.width = circle.radius * 2F;
+        this.height = circle.radius * 2F;
     }
 
     public void set(Vector2 position, Vector2 size) {
@@ -153,7 +144,6 @@ public class Ellipse implements Serializable, Shape2D {
 
     /**
      * Sets the x and y-coordinates of ellipse center from a {@link Vector2}.
-     *
      * @param position The position vector of the center of the ellipse
      * @return this ellipse for chaining
      */
@@ -166,7 +156,6 @@ public class Ellipse implements Serializable, Shape2D {
 
     /**
      * Sets the x and y-coordinates of ellipse center
-     *
      * @param x The x-coordinate of the center of the ellipse
      * @param y The y-coordinate of the center of the ellipse
      * @return this ellipse for chaining
@@ -179,11 +168,10 @@ public class Ellipse implements Serializable, Shape2D {
     }
 
     /**
-     * Sets the width and height of this ellipse
-     *
-     * @param width  The width
-     * @param height The height
-     * @return this ellipse for chaining
+     * Sets the width and height of this ellipse.
+     * @param width  The width.
+     * @param height The height.
+     * @return this ellipse for chaining.
      */
     public Ellipse setSize(float width, float height) {
         this.width = width;
@@ -193,28 +181,26 @@ public class Ellipse implements Serializable, Shape2D {
     }
 
     /**
-     * @return The area of this {@link Ellipse} as {@link MathUtils#PI} * {@link Ellipse#width} * {@link Ellipse#height}
+     * @return The area of this {@link Ellipse} as {@link MathUtils#PI} * {@link Ellipse#width} * {@link Ellipse#height}.
      */
     public float area() {
         return MathUtils.PI * (this.width * this.height) / 4;
     }
 
     /**
-     * Approximates the circumference of this {@link Ellipse}. Oddly enough, the circumference of an ellipse is actually difficult
-     * to compute exactly.
-     *
-     * @return The Ramanujan approximation to the circumference of an ellipse if one dimension is at least three times longer than
-     * the other, else the simpler approximation
+     * Approximates the circumference of this {@link Ellipse}. Oddly enough, the circumference of an ellipse is actually difficult to compute exactly.
+     * @return The Ramanujan approximation to the circumference of an ellipse if one dimension is at least three times longer than the other, else the simpler
+     * approximation.
      */
     public float circumference() {
-        float a = this.width / 2;
-        float b = this.height / 2;
+        float a = this.width / 2F;
+        float b = this.height / 2F;
         if (a * 3 > b || b * 3 > a) {
             // If one dimension is three times as long as the other...
-            return (float) (MathUtils.PI * ((3 * (a + b)) - Math.sqrt((3 * a + b) * (a + 3 * b))));
+            return (float) (MathUtils.PI * ((3 * (a + b)) - Math.sqrt((3 * a + b) * (a + 3 * b)))); // This is Ramanujan's first approximation.
         } else {
             // We can use the simpler approximation, then
-            return (float) (MathUtils.PI2 * Math.sqrt((a * a + b * b) / 2));
+            return (float) (MathUtils.PI2 * Math.sqrt((a * a + b * b) / 2)); // This is Euler's approximation.
         }
     }
 
@@ -227,10 +213,10 @@ public class Ellipse implements Serializable, Shape2D {
     }
 
     @Override
-    public boolean equals(Object o) {
-        if (o == this) return true;
-        if (o == null || o.getClass() != this.getClass()) return false;
-        Ellipse e = (Ellipse) o;
+    public boolean equals(Object otherObject) {
+        if (otherObject == this) return true;
+        if (otherObject == null || otherObject.getClass() != this.getClass()) return false;
+        Ellipse e = (Ellipse) otherObject;
         return this.x == e.x && this.y == e.y && this.width == e.width && this.height == e.height;
     }
 
