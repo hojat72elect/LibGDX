b65413e12ca1f5e811f690c26705566a5394ef20
hojat72elect <hojat72elect@gmail.com>
2025-12-16 01:39:41 -0500

    Cleaned up.
    #9

--- gdx/test/com/badlogic/gdx/graphics/g3d/utils/AnimationControllerTest.java
+++ gdx/test/com/badlogic/gdx/graphics/g3d/utils/AnimationControllerTest.java
@@ -1,8 +1,5 @@
 package com.badlogic.gdx.graphics.g3d.utils;
 
-import org.junit.Assert;
-import org.junit.Test;
-
 import com.badlogic.gdx.graphics.g3d.Model;
 import com.badlogic.gdx.graphics.g3d.ModelInstance;
 import com.badlogic.gdx.graphics.g3d.model.Animation;
@@ -10,116 +7,98 @@ import com.badlogic.gdx.graphics.g3d.model.NodeKeyframe;
 import com.badlogic.gdx.graphics.g3d.utils.AnimationController.AnimationDesc;
 import com.badlogic.gdx.utils.Array;
 
+import org.junit.Assert;
+import org.junit.Test;
+
 public class AnimationControllerTest {
 
-	@Test
-	public void testGetFirstKeyframeIndexAtTimeNominal () {
+    @Test
+    public void testGetFirstKeyframeIndexAtTimeNominal() {
 
-		Array<NodeKeyframe<String>> keyFrames = new Array<NodeKeyframe<String>>();
+        Array<NodeKeyframe<String>> keyFrames = new Array<>();
+        keyFrames.add(new NodeKeyframe<>(0f, "1st"));
+        keyFrames.add(new NodeKeyframe<>(3f, "2nd"));
+        keyFrames.add(new NodeKeyframe<>(12f, "3rd"));
+        keyFrames.add(new NodeKeyframe<>(13f, "4th"));
+        Assert.assertEquals(0, BaseAnimationController.getFirstKeyframeIndexAtTime(keyFrames, -1f));
+        Assert.assertEquals(0, BaseAnimationController.getFirstKeyframeIndexAtTime(keyFrames, 0f));
+        Assert.assertEquals(0, BaseAnimationController.getFirstKeyframeIndexAtTime(keyFrames, 2f));
+        Assert.assertEquals(1, BaseAnimationController.getFirstKeyframeIndexAtTime(keyFrames, 9f));
+        Assert.assertEquals(2, BaseAnimationController.getFirstKeyframeIndexAtTime(keyFrames, 12.5f));
+        Assert.assertEquals(2, BaseAnimationController.getFirstKeyframeIndexAtTime(keyFrames, 13f));
+        Assert.assertEquals(0, BaseAnimationController.getFirstKeyframeIndexAtTime(keyFrames, 14f));
+    }
 
-		keyFrames.add(new NodeKeyframe<String>(0f, "1st"));
-		keyFrames.add(new NodeKeyframe<String>(3f, "2nd"));
-		keyFrames.add(new NodeKeyframe<String>(12f, "3rd"));
-		keyFrames.add(new NodeKeyframe<String>(13f, "4th"));
+    @Test
+    public void testGetFirstKeyframeIndexAtTimeSingleKey() {
 
-		Assert.assertEquals(0, BaseAnimationController.getFirstKeyframeIndexAtTime(keyFrames, -1f));
-		Assert.assertEquals(0, BaseAnimationController.getFirstKeyframeIndexAtTime(keyFrames, 0f));
-		Assert.assertEquals(0, BaseAnimationController.getFirstKeyframeIndexAtTime(keyFrames, 2f));
-		Assert.assertEquals(1, BaseAnimationController.getFirstKeyframeIndexAtTime(keyFrames, 9f));
-		Assert.assertEquals(2, BaseAnimationController.getFirstKeyframeIndexAtTime(keyFrames, 12.5f));
-		Assert.assertEquals(2, BaseAnimationController.getFirstKeyframeIndexAtTime(keyFrames, 13f));
-		Assert.assertEquals(0, BaseAnimationController.getFirstKeyframeIndexAtTime(keyFrames, 14f));
-	}
+        Array<NodeKeyframe<String>> keyFrames = new Array<>();
+        keyFrames.add(new NodeKeyframe<>(10f, "1st"));
+        Assert.assertEquals(0, BaseAnimationController.getFirstKeyframeIndexAtTime(keyFrames, 9f));
+        Assert.assertEquals(0, BaseAnimationController.getFirstKeyframeIndexAtTime(keyFrames, 10f));
+        Assert.assertEquals(0, BaseAnimationController.getFirstKeyframeIndexAtTime(keyFrames, 11f));
+    }
 
-	@Test
-	public void testGetFirstKeyframeIndexAtTimeSingleKey () {
+    @Test
+    public void testGetFirstKeyframeIndexAtTimeEmpty() {
 
-		Array<NodeKeyframe<String>> keyFrames = new Array<NodeKeyframe<String>>();
+        Array<NodeKeyframe<String>> keyFrames = new Array<>();
+        Assert.assertEquals(0, BaseAnimationController.getFirstKeyframeIndexAtTime(keyFrames, 3f));
+    }
 
-		keyFrames.add(new NodeKeyframe<String>(10f, "1st"));
+    private static void assertSameAnimation(Animation expected, AnimationDesc actual) {
+        if (!expected.id.equals(actual.animation.id)) {
+            Assert.fail("expected: " + expected.id + ", actual: " + actual.animation.id);
+        }
+    }
 
-		Assert.assertEquals(0, BaseAnimationController.getFirstKeyframeIndexAtTime(keyFrames, 9f));
-		Assert.assertEquals(0, BaseAnimationController.getFirstKeyframeIndexAtTime(keyFrames, 10f));
-		Assert.assertEquals(0, BaseAnimationController.getFirstKeyframeIndexAtTime(keyFrames, 11f));
-	}
+    @Test
+    public void testEndUpActionAtDurationTime() {
 
-	@Test
-	public void testGetFirstKeyframeIndexAtTimeEmpty () {
+        Animation loop = new Animation();
+        loop.id = "loop";
+        loop.duration = 1f;
+        Animation action = new Animation();
+        action.id = "action";
+        action.duration = 0.2f;
+        ModelInstance modelInstance = new ModelInstance(new Model());
+        modelInstance.animations.add(loop);
+        modelInstance.animations.add(action);
+        AnimationController animationController = new AnimationController(modelInstance);
+        animationController.setAnimation("loop", -1);
+        assertSameAnimation(loop, animationController.current);
+        animationController.update(1);
+        assertSameAnimation(loop, animationController.current);
+        animationController.update(0.01f);
+        assertSameAnimation(loop, animationController.current);
+        animationController.action("action", 1, 1f, null, 0f);
+        assertSameAnimation(action, animationController.current);
+        animationController.update(0.2f);
+        assertSameAnimation(loop, animationController.current);
+    }
 
-		Array<NodeKeyframe<String>> keyFrames = new Array<NodeKeyframe<String>>();
+    @Test
+    public void testEndUpActionAtDurationTimeReverse() {
 
-		Assert.assertEquals(0, BaseAnimationController.getFirstKeyframeIndexAtTime(keyFrames, 3f));
-	}
-
-	private static void assertSameAnimation (Animation expected, AnimationDesc actual) {
-		if (!expected.id.equals(actual.animation.id)) {
-			Assert.fail("expected: " + expected.id + ", actual: " + actual.animation.id);
-		}
-	}
-
-	@Test
-	public void testEndUpActionAtDurationTime () {
-
-		Animation loop = new Animation();
-		loop.id = "loop";
-		loop.duration = 1f;
-
-		Animation action = new Animation();
-		action.id = "action";
-		action.duration = 0.2f;
-
-		ModelInstance modelInstance = new ModelInstance(new Model());
-		modelInstance.animations.add(loop);
-		modelInstance.animations.add(action);
-
-		AnimationController animationController = new AnimationController(modelInstance);
-
-		animationController.setAnimation("loop", -1);
-		assertSameAnimation(loop, animationController.current);
-
-		animationController.update(1);
-		assertSameAnimation(loop, animationController.current);
-
-		animationController.update(0.01f);
-		assertSameAnimation(loop, animationController.current);
-
-		animationController.action("action", 1, 1f, null, 0f);
-		assertSameAnimation(action, animationController.current);
-
-		animationController.update(0.2f);
-		assertSameAnimation(loop, animationController.current);
-	}
-
-	@Test
-	public void testEndUpActionAtDurationTimeReverse () {
-
-		Animation loop = new Animation();
-		loop.id = "loop";
-		loop.duration = 1f;
-
-		Animation action = new Animation();
-		action.id = "action";
-		action.duration = 0.2f;
-
-		ModelInstance modelInstance = new ModelInstance(new Model());
-		modelInstance.animations.add(loop);
-		modelInstance.animations.add(action);
-
-		AnimationController animationController = new AnimationController(modelInstance);
-
-		animationController.setAnimation("loop", -1, -1f, null);
-		assertSameAnimation(loop, animationController.current);
-
-		animationController.update(1);
-		assertSameAnimation(loop, animationController.current);
-
-		animationController.update(0.01f);
-		assertSameAnimation(loop, animationController.current);
-
-		animationController.action("action", 1, -1f, null, 0f);
-		assertSameAnimation(action, animationController.current);
-
-		animationController.update(0.2f);
-		assertSameAnimation(loop, animationController.current);
-	}
+        Animation loop = new Animation();
+        loop.id = "loop";
+        loop.duration = 1f;
+        Animation action = new Animation();
+        action.id = "action";
+        action.duration = 0.2f;
+        ModelInstance modelInstance = new ModelInstance(new Model());
+        modelInstance.animations.add(loop);
+        modelInstance.animations.add(action);
+        AnimationController animationController = new AnimationController(modelInstance);
+        animationController.setAnimation("loop", -1, -1f, null);
+        assertSameAnimation(loop, animationController.current);
+        animationController.update(1);
+        assertSameAnimation(loop, animationController.current);
+        animationController.update(0.01f);
+        assertSameAnimation(loop, animationController.current);
+        animationController.action("action", 1, -1f, null, 0f);
+        assertSameAnimation(action, animationController.current);
+        animationController.update(0.2f);
+        assertSameAnimation(loop, animationController.current);
+    }
 }
