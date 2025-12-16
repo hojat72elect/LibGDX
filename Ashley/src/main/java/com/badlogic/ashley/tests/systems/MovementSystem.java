54bf9dfcdc32e8716d94782da1e6d4bd33b52d74
hojat72elect <hojat72elect@gmail.com>
2025-12-16 17:10:52 -0500

    Added the practical tests of Ashley.
    #8

---
+++ Ashley/src/main/java/com/badlogic/ashley/tests/systems/MovementSystem.java
@@ -0,0 +1,26 @@
+package com.badlogic.ashley.tests.systems;
+
+import com.badlogic.ashley.core.ComponentMapper;
+import com.badlogic.ashley.core.Entity;
+import com.badlogic.ashley.core.Family;
+import com.badlogic.ashley.systems.IteratingSystem;
+import com.badlogic.ashley.tests.components.MovementComponent;
+import com.badlogic.ashley.tests.components.PositionComponent;
+
+public class MovementSystem extends IteratingSystem {
+    private final ComponentMapper<PositionComponent> pm = ComponentMapper.getFor(PositionComponent.class);
+    private final ComponentMapper<MovementComponent> mm = ComponentMapper.getFor(MovementComponent.class);
+
+    public MovementSystem() {
+        super(Family.all(PositionComponent.class, MovementComponent.class).get());
+    }
+
+    @Override
+    public void processEntity(Entity entity, float deltaTime) {
+        PositionComponent position = pm.get(entity);
+        MovementComponent movement = mm.get(entity);
+
+        position.x += movement.velocityX * deltaTime;
+        position.y += movement.velocityY * deltaTime;
+    }
+}
---
+++ Ashley/src/main/java/com/badlogic/ashley/tests/systems/RenderSystem.java
@@ -0,0 +1,58 @@
+package com.badlogic.ashley.tests.systems;
+
+import com.badlogic.ashley.core.ComponentMapper;
+import com.badlogic.ashley.core.Engine;
+import com.badlogic.ashley.core.Entity;
+import com.badlogic.ashley.core.EntitySystem;
+import com.badlogic.ashley.core.Family;
+import com.badlogic.ashley.tests.components.PositionComponent;
+import com.badlogic.ashley.tests.components.VisualComponent;
+import com.badlogic.ashley.utils.ImmutableArray;
+import com.badlogic.gdx.graphics.OrthographicCamera;
+import com.badlogic.gdx.graphics.g2d.SpriteBatch;
+
+public class RenderSystem extends EntitySystem {
+    private final SpriteBatch batch;
+    private final OrthographicCamera camera;
+    private final ComponentMapper<PositionComponent> pm = ComponentMapper.getFor(PositionComponent.class);
+    private final ComponentMapper<VisualComponent> vm = ComponentMapper.getFor(VisualComponent.class);
+    private ImmutableArray<Entity> entities;
+
+    public RenderSystem(OrthographicCamera camera) {
+        batch = new SpriteBatch();
+
+        this.camera = camera;
+    }
+
+    @Override
+    public void addedToEngine(Engine engine) {
+        entities = engine.getEntitiesFor(Family.all(PositionComponent.class, VisualComponent.class).get());
+    }
+
+    @Override
+    public void removedFromEngine(Engine engine) {
+
+    }
+
+    @Override
+    public void update(float deltaTime) {
+        PositionComponent position;
+        VisualComponent visual;
+
+        camera.update();
+
+        batch.begin();
+        batch.setProjectionMatrix(camera.combined);
+
+        for (int i = 0; i < entities.size(); ++i) {
+            Entity e = entities.get(i);
+
+            position = pm.get(e);
+            visual = vm.get(e);
+
+            batch.draw(visual.region, position.x, position.y);
+        }
+
+        batch.end();
+    }
+}
