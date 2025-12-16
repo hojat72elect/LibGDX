878b442093281b550259fc93e76dfe09176ddf27
hojat72elect <hojat72elect@gmail.com>
2025-12-16 01:38:09 -0500

    AnimationDescTest.java -> kt
    #9

--- gdx/test/com/badlogic/gdx/graphics/g3d/utils/AnimationDescTest.kt
+++ gdx/test/com/badlogic/gdx/graphics/g3d/utils/AnimationDescTest.kt
@@ -13,38 +13,38 @@ class AnimationDescTest {
     fun setup() {
         anim = AnimationDesc()
         anim.animation = Animation()
-        anim.duration = 1f
+        anim.duration = 1F
         anim.listener = null
         anim.loopCount = 1
-        anim.offset = 0f
-        anim.speed = 1f
-        anim.time = 0f
+        anim.offset = 0F
+        anim.speed = 1F
+        anim.time = 0F
     }
 
     @Test
     fun testUpdateNominal() {
-        Assert.assertEquals(-1f, anim.update(.75f), EPSILON)
-        Assert.assertEquals(.5f, anim.update(.75f), EPSILON)
-        Assert.assertEquals(.75f, anim.update(.75f), EPSILON)
+        Assert.assertEquals(-1F, anim.update(.75f), EPSILON)
+        Assert.assertEquals(.5F, anim.update(.75f), EPSILON)
+        Assert.assertEquals(.75F, anim.update(.75f), EPSILON)
     }
 
     @Test
     fun testUpdateJustEnd() {
-        Assert.assertEquals(-1f, anim.update(.5f), EPSILON)
-        Assert.assertEquals(0f, anim.update(.5f), EPSILON)
-        Assert.assertEquals(.5f, anim.update(.5f), EPSILON)
+        Assert.assertEquals(-1F, anim.update(.5f), EPSILON)
+        Assert.assertEquals(0F, anim.update(.5f), EPSILON)
+        Assert.assertEquals(.5F, anim.update(.5f), EPSILON)
     }
 
     @Test
     fun testUpdateBigDelta() {
-        Assert.assertEquals(4.2f, anim.update(5.2f), EPSILON)
-        Assert.assertEquals(7.3f, anim.update(7.3f), EPSILON)
+        Assert.assertEquals(4.2F, anim.update(5.2f), EPSILON)
+        Assert.assertEquals(7.3F, anim.update(7.3f), EPSILON)
     }
 
     @Test
     fun testUpdateZeroDelta() {
-        Assert.assertEquals(-1f, anim.update(0f), EPSILON)
-        Assert.assertEquals(0f, anim.time, EPSILON)
+        Assert.assertEquals(-1F, anim.update(0f), EPSILON)
+        Assert.assertEquals(0F, anim.time, EPSILON)
     }
 
     @Test
@@ -52,9 +52,9 @@ class AnimationDescTest {
         anim.speed = -1f
         anim.time = anim.duration
 
-        Assert.assertEquals(-1f, anim.update(.75f), EPSILON)
-        Assert.assertEquals(.5f, anim.update(.75f), EPSILON)
-        Assert.assertEquals(.75f, anim.update(.75f), EPSILON)
+        Assert.assertEquals(-1F, anim.update(.75f), EPSILON)
+        Assert.assertEquals(.5F, anim.update(.75f), EPSILON)
+        Assert.assertEquals(.75F, anim.update(.75f), EPSILON)
     }
 
     @Test
@@ -62,9 +62,9 @@ class AnimationDescTest {
         anim.speed = -1f
         anim.time = anim.duration
 
-        Assert.assertEquals(-1f, anim.update(.5f), EPSILON)
-        Assert.assertEquals(0f, anim.update(.5f), EPSILON)
-        Assert.assertEquals(.5f, anim.update(.5f), EPSILON)
+        Assert.assertEquals(-1F, anim.update(.5f), EPSILON)
+        Assert.assertEquals(0F, anim.update(.5f), EPSILON)
+        Assert.assertEquals(.5F, anim.update(.5f), EPSILON)
     }
 
     @Test
@@ -72,8 +72,8 @@ class AnimationDescTest {
         anim.speed = -1f
         anim.time = anim.duration
 
-        Assert.assertEquals(4.2f, anim.update(5.2f), EPSILON)
-        Assert.assertEquals(7.3f, anim.update(7.3f), EPSILON)
+        Assert.assertEquals(4.2F, anim.update(5.2f), EPSILON)
+        Assert.assertEquals(7.3F, anim.update(7.3f), EPSILON)
     }
 
     @Test
@@ -81,7 +81,7 @@ class AnimationDescTest {
         anim.speed = -1f
         anim.time = anim.duration
 
-        Assert.assertEquals(-1f, anim.update(0f), EPSILON)
+        Assert.assertEquals(-1F, anim.update(0f), EPSILON)
         Assert.assertEquals(anim.duration, anim.time, EPSILON)
     }
 
