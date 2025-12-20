c775709f0e8ab69cac44380d8debf36d9cdafe33
hojat72elect <hojat72elect@gmail.com>
2025-12-20 18:53:19 -0500

    Finally managed to add gdx-vfx to this project.
    closes #12

---
+++ gdx-vfx/src/main/java/com/crashinvaders/vfx/demo/desktop/DesktopLauncher.java
@@ -0,0 +1,22 @@
+package com.crashinvaders.vfx.demo.desktop;
+
+import com.badlogic.gdx.Files;
+import com.badlogic.gdx.backends.lwjgl3.Lwjgl3Application;
+import com.badlogic.gdx.backends.lwjgl3.Lwjgl3ApplicationConfiguration;
+import com.crashinvaders.vfx.demo.App;
+
+public class DesktopLauncher {
+    public static void main(String[] arg) {
+        Lwjgl3ApplicationConfiguration config = new Lwjgl3ApplicationConfiguration();
+        config.setTitle("GDX VFX Demo");
+        config.setWindowIcon(Files.FileType.Classpath,
+                "gdx-vfx-icon16.png",
+                "gdx-vfx-icon32.png",
+                "gdx-vfx-icon64.png",
+                "gdx-vfx-icon128.png");
+        config.setWindowedMode(640, 480);
+        config.setWindowSizeLimits(32, 32, -1, -1);
+
+        new Lwjgl3Application(new App(), config);
+    }
+}
