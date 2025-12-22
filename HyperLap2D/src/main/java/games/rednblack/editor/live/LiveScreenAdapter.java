65b178c2ea98f221de1d132c1df934170f36b40a
hojat72elect <hojat72elect@gmail.com>
2025-12-21 23:48:17 -0500

    Added the main part of the source code of the hyperlap2d editor (part 9).
    #13

---
+++ HyperLap2D/src/main/java/games/rednblack/editor/live/LiveScreenAdapter.java
@@ -0,0 +1,20 @@
+package games.rednblack.editor.live;
+
+import com.badlogic.gdx.Game;
+
+public class LiveScreenAdapter extends Game {
+
+    private LivePreviewScreen livePreviewScreen;
+
+    @Override
+    public void create() {
+        livePreviewScreen = new LivePreviewScreen();
+        setScreen(livePreviewScreen);
+    }
+
+    @Override
+    public void dispose() {
+        super.dispose();
+        livePreviewScreen.dispose();
+    }
+}
