5be98a6b704905d52a188d4693d101ca93bce65a
hojat72elect <hojat72elect@gmail.com>
2025-12-22 02:04:15 -0500

    Added the main part of the source code of the hyperlap2d editor (part 24).
    #13

---
+++ HyperLap2D/src/main/java/games/rednblack/editor/system/ParticleContinuousSystem.java
@@ -0,0 +1,28 @@
+package games.rednblack.editor.system;
+
+import com.artemis.annotations.All;
+import com.badlogic.gdx.graphics.g2d.ParticleEffect;
+
+import games.rednblack.editor.renderer.components.particle.ParticleComponent;
+import games.rednblack.editor.renderer.systems.ParticleSystem;
+
+/**
+ * Some particle panels might not be continuous, so they will stop after first iteration, which is ok
+ * This system will make sure they look continuous while in editor, so user will find and see them easily.
+ *
+ */
+@All(ParticleComponent.class)
+public class ParticleContinuousSystem extends ParticleSystem {
+
+    @Override
+    protected void process(int entity) {
+        super.process(entity);
+
+        ParticleComponent particleComponent = particleComponentMapper.get(entity);
+        ParticleEffect particleEffect = particleComponent.particleEffect;
+
+        if (particleEffect.isComplete()) {
+            particleEffect.reset(false);
+        }
+    }
+}
---
+++ HyperLap2D/src/main/java/games/rednblack/editor/system/TalosContinuousSystem.java
@@ -0,0 +1,28 @@
+package games.rednblack.editor.system;
+
+import com.artemis.annotations.All;
+
+import games.rednblack.h2d.extension.talos.TalosComponent;
+import games.rednblack.h2d.extension.talos.TalosSystem;
+import games.rednblack.talos.runtime.ParticleEffectInstance;
+
+/**
+ * Some particle panels might not be continuous, so they will stop after first iteration, which is ok
+ * This system will make sure they look continuous while in editor, so user will find and see them easily.
+ *
+ */
+@All({TalosComponent.class})
+public class TalosContinuousSystem extends TalosSystem {
+
+    @Override
+    protected void process(int entity) {
+        super.process(entity);
+
+        TalosComponent talosComponent = particleComponentMapper.get(entity);
+        ParticleEffectInstance effect = talosComponent.effect;
+
+        if (!effect.isContinuous() && effect.isComplete()) {
+            effect.restart();
+        }
+    }
+}
