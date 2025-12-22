58d93c5df2469d7c5025ba87dc73570f3366276c
hojat72elect <hojat72elect@gmail.com>
2025-12-21 22:40:12 -0500

    Added source code of the "plugin-performance" module to this project.
    #13

---
+++ HyperLap2D/src/main/java/games/rednblack/editor/plugin/performance/PerformancePlugin.java
@@ -0,0 +1,26 @@
+package games.rednblack.editor.plugin.performance;
+
+import net.mountainblade.modular.annotations.Implementation;
+
+import games.rednblack.h2d.common.MenuAPI;
+import games.rednblack.h2d.common.plugins.H2DPluginAdapter;
+
+@Implementation(authors = "azakhary", version = "0.0.1")
+public class PerformancePlugin extends H2DPluginAdapter {
+    public static final String CLASS_NAME = "games.rednblack.editor.plugin.performance";
+
+    public static final String PANEL_OPEN = CLASS_NAME + ".PANEL_OPEN";
+
+    private final PerformancePanelMediator performancePanelMediator;
+
+    public PerformancePlugin() {
+        super(CLASS_NAME);
+        performancePanelMediator = new PerformancePanelMediator(this);
+    }
+
+    @Override
+    public void initPlugin() {
+        facade.registerMediator(performancePanelMediator);
+        pluginAPI.addMenuItem(MenuAPI.WINDOW_MENU, "Performance", PANEL_OPEN);
+    }
+}
