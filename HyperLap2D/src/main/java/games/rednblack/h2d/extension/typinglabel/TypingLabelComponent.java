c8cb32c741fc86eb31b44815937ef4abff6b5da4
hojat72elect <hojat72elect@gmail.com>
2025-12-21 22:21:39 -0500

    Added source code of the "h2d-libgdx-typinglabel-extension" module to this project.
    #13

---
+++ HyperLap2D/src/main/java/games/rednblack/h2d/extension/typinglabel/TypingLabelComponent.java
@@ -0,0 +1,33 @@
+package games.rednblack.h2d.extension.typinglabel;
+
+import com.artemis.PooledComponent;
+import com.badlogic.gdx.utils.CharArray;
+import com.github.tommyettinger.textra.Styles;
+import com.github.tommyettinger.textra.TypingLabel;
+
+public class TypingLabelComponent extends PooledComponent {
+
+    private final CharArray originalText = new CharArray();
+    public transient TypingLabel typingLabel;
+    public transient Styles.LabelStyle labelStyle;
+
+    public CharArray getOriginalText() {
+        return originalText;
+    }
+
+    public void setOriginalText(CharArray originalText) {
+        this.originalText.setLength(0);
+        this.originalText.append(originalText);
+    }
+
+    @Override
+    public void reset() {
+        if (typingLabel != null)
+            typingLabel.remove();
+        typingLabel = null;
+        if (labelStyle != null)
+            labelStyle.font.dispose();
+        labelStyle = null;
+        originalText.setLength(0);
+    }
+}
