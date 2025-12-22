69a41528fad1d8fb845472d5717989f32316cfbf
hojat72elect <hojat72elect@gmail.com>
2025-12-22 02:05:06 -0500

    Added the main part of the source code of the hyperlap2d editor (part 25).
    #13

---
+++ HyperLap2D/src/main/java/games/rednblack/editor/splash/SplashMediator.java
@@ -0,0 +1,91 @@
+package games.rednblack.editor.splash;
+
+import com.badlogic.gdx.Gdx;
+import com.badlogic.gdx.utils.Json;
+
+import org.lwjgl.util.tinyfd.TinyFileDialogs;
+
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
+
+import games.rednblack.editor.HyperLap2DApp;
+import games.rednblack.editor.renderer.utils.Version;
+import games.rednblack.editor.utils.AppConfig;
+import games.rednblack.h2d.common.network.HttpDownloadUtility;
+import games.rednblack.h2d.common.network.model.GithubReleaseData;
+import games.rednblack.h2d.common.network.model.SnapshotReleaseData;
+import games.rednblack.puremvc.Mediator;
+import games.rednblack.puremvc.interfaces.INotification;
+import games.rednblack.puremvc.util.Interests;
+
+public class SplashMediator extends Mediator<Object> {
+
+    private static final String TAG = SplashMediator.class.getCanonicalName();
+    private static final String NAME = TAG;
+
+    public SplashMediator() {
+        super(NAME, new Object());
+    }
+
+    @Override
+    public void listNotificationInterests(Interests interests) {
+        interests.add(SplashScreenAdapter.UPDATE_SPLASH,
+                SplashScreenAdapter.CLOSE_SPLASH);
+    }
+
+    @Override
+    public void handleNotification(INotification notification) {
+        super.handleNotification(notification);
+        System.out.println(notification.getBody().toString());
+
+        SplashScreenAdapter splash = (SplashScreenAdapter) (HyperLap2DApp.getInstance().splashWindow.getListener());
+
+        if (HyperLap2DApp.getInstance().splashWindow != null && HyperLap2DApp.getInstance().mainWindow != null) {
+            splash.setProgressStatus(notification.getBody().toString());
+
+            if (notification.getName().equals(SplashScreenAdapter.CLOSE_SPLASH)) {
+                ExecutorService executor = Executors.newSingleThreadExecutor();
+                executor.execute(() -> {
+                    splash.setProgressStatus("Checking for updates...");
+                    try {
+                        Version latestVer, currVer;
+                        if (AppConfig.getInstance().build != null) {
+                            String data = HttpDownloadUtility.downloadToString("https://hyperlap2d.rednblack.games/upload/snapshots/snapshot.json");
+                            Json json = new Json();
+                            json.setIgnoreUnknownFields(true);
+                            SnapshotReleaseData jsonData = json.fromJson(SnapshotReleaseData.class, data);
+                            latestVer = new Version(String.valueOf(jsonData.build));
+                            currVer = new Version(AppConfig.getInstance().build);
+                        } else {
+                            String data = HttpDownloadUtility.downloadToString("https://api.github.com/repos/rednblackgames/HyperLap2D/releases/latest");
+                            Json json = new Json();
+                            json.setIgnoreUnknownFields(true);
+                            GithubReleaseData jsonData = json.fromJson(GithubReleaseData.class, data);
+                            latestVer = new Version(jsonData.tag_name.replace("v", ""));
+                            currVer = AppConfig.getInstance().version;
+                        }
+
+                        if (latestVer.compareTo(currVer) > 0) {
+                            boolean result = TinyFileDialogs.tinyfd_messageBox("New update found!",
+                                    "A new version of HyperLap2D has been found " + latestVer.get() + " (current: " + currVer.get() + "), would you like to download it?",
+                                    "yesno", "info", true);
+                            if (result) {
+                                Gdx.net.openURI("https://hyperlap2d.rednblack.games/download");
+                            }
+                        }
+                    } catch (Exception e) {
+                        e.printStackTrace();
+                    }
+
+                    Gdx.app.postRunnable(() -> {
+                        splash.loadedData();
+                        HyperLap2DApp.getInstance().mainWindow.setVisible(true);
+                    });
+                });
+                executor.shutdown();
+            }
+
+            HyperLap2DApp.getInstance().splashWindow.focusWindow();
+        }
+    }
+}
---
+++ HyperLap2D/src/main/java/games/rednblack/editor/splash/SplashStage.java
@@ -0,0 +1,94 @@
+package games.rednblack.editor.splash;
+
+import com.badlogic.gdx.Gdx;
+import com.badlogic.gdx.graphics.Color;
+import com.badlogic.gdx.graphics.g2d.BitmapFont;
+import com.badlogic.gdx.graphics.g2d.TextureAtlas;
+import com.badlogic.gdx.scenes.scene2d.Stage;
+import com.badlogic.gdx.scenes.scene2d.actions.Actions;
+import com.badlogic.gdx.scenes.scene2d.ui.Image;
+import com.badlogic.gdx.scenes.scene2d.ui.Label;
+
+import games.rednblack.editor.HyperLap2DApp;
+import games.rednblack.editor.utils.AppConfig;
+
+public class SplashStage extends Stage {
+    private final TextureAtlas atlas;
+    private final Image logoText;
+    private final Label progress;
+    private boolean isLoading = true;
+
+    public SplashStage(boolean isLoading) {
+        this.isLoading = isLoading;
+        atlas = new TextureAtlas(Gdx.files.internal("splash/splash.atlas"));
+        BitmapFont robotFont = new BitmapFont(Gdx.files.internal("splash/roboto.fnt"));
+        Label.LabelStyle whiteLabelStyle = new Label.LabelStyle(robotFont, new Color(1, 1, 1, 1f));
+        Label.LabelStyle blackLabelStyle = new Label.LabelStyle(robotFont, new Color(0, 0, 0, 1f));
+
+        Image bg = new Image(atlas.findRegion("bg_color"));
+        bg.setColor(0, 0, 0, 1);
+        bg.setX(0);
+        bg.setY(0);
+        bg.setWidth(getWidth());
+        bg.setHeight(getHeight());
+        addActor(bg);
+
+        Image imageBg = new Image(atlas.findRegion("splash_bg"));
+        imageBg.setX(0);
+        imageBg.setY(getHeight() - imageBg.getHeight());
+        addActor(imageBg);
+
+        Image logo = new Image(atlas.findRegion("splash_logo"));
+        logo.setX(25);
+        logo.setY(getHeight() - logo.getHeight() - 20);
+        addActor(logo);
+
+        logoText = new Image(atlas.findRegion("splash_logo_text"));
+        logoText.setX(200);
+        logoText.setY(logo.getY() + 30);
+        addActor(logoText);
+
+        progress = new Label("Loading fonts", blackLabelStyle);
+        progress.setX(logoText.getX() + ((logoText.getWidth() - progress.getWidth()) / 2));
+        progress.setY(logo.getY() + 5);
+        if (isLoading)
+            addActor(progress);
+
+        Label companyName = new Label("Red & Black Games", whiteLabelStyle);
+        companyName.setX(13);
+        companyName.setY(55 - companyName.getHeight() - 7);
+        addActor(companyName);
+
+        Label copyright = new Label("Copyright (c) 2025,  All rights reserved.", whiteLabelStyle);
+        copyright.setX(13);
+        copyright.setY(companyName.getY() - 20);
+        addActor(copyright);
+
+        Label version = new Label("v" + AppConfig.getInstance().versionString, whiteLabelStyle);
+        version.setX(getWidth() - 13 - version.getWidth());
+        version.setY(companyName.getY());
+        addActor(version);
+
+        setProgressStatus("Initializing");
+    }
+
+    @Override
+    public boolean touchUp(int screenX, int screenY, int pointer, int button) {
+        super.touchUp(screenX, screenY, pointer, button);
+        if (!isLoading) {
+            HyperLap2DApp.getInstance().splashWindow.closeWindow();
+        }
+        return true;
+    }
+
+    public void setProgressStatus(String status) {
+        progress.setText(status);
+        progress.setX(logoText.getX() + ((logoText.getWidth() - progress.getPrefWidth()) / 2));
+    }
+
+    public void loadedData() {
+        addAction(Actions.sequence(Actions.delay(0.8f), Actions.run(() -> {
+            HyperLap2DApp.getInstance().splashWindow.closeWindow();
+        })));
+    }
+}
