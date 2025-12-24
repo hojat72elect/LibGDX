84b35cce9f2a6014566dbdaee34588498b4fc9e8
hojat72elect <hojat72elect@gmail.com>
2025-12-24 05:53:23 -0500

    Added source code of the "editor" module (part 6).
    #16

---
+++ TalosVFX/src/main/java/com/talosvfx/talos/editor/project2/apps/SingletonApp.java
@@ -0,0 +1,8 @@
+package com.talosvfx.talos.editor.project2.apps;
+
+import java.lang.annotation.Retention;
+import java.lang.annotation.RetentionPolicy;
+
+@Retention(RetentionPolicy.RUNTIME)
+public @interface SingletonApp {
+}
