f8af9689fc65b8339ec441acc46b37d34481b4bb
hojat72elect <hojat72elect@gmail.com>
2026-01-01 19:41:57 -0500

    Moved ComparableTimSort.java to kerman game engine.

--- gdx/src/com/badlogic/gdx/utils/ComparableTimSort.java
+++ gdx/src/com/badlogic/gdx/utils/ComparableTimSort.java
@@ -1,16 +1,3 @@
-/*
- * Copyright (C) 2008 The Android Open Source Project
- *
- * Licensed under the Apache License, Version 2.0 (the "License"); you may not use this file except in compliance with the
- * License. You may obtain a copy of the License at
- *
- * http://www.apache.org/licenses/LICENSE-2.0
- *
- * Unless required by applicable law or agreed to in writing, software distributed under the License is distributed on an "AS IS"
- * BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the License for the specific language
- * governing permissions and limitations under the License.
- */
-
 package com.badlogic.gdx.utils;
 
 /**
@@ -21,6 +8,8 @@ package com.badlogic.gdx.utils;
  * If you are using an optimizing VM, you may find that ComparableTimSort offers no performance benefit over TimSort in
  * conjunction with a comparator that simply returns {@code ((Comparable)first).compareTo(Second)}. If this is the case, you are
  * better off deleting ComparableTimSort to eliminate the code duplication. (See Arrays.java for details.)
+ * <p>
+ * Info : Moved this class to Kerman Game Engine.
  */
 class ComparableTimSort {
     /**
