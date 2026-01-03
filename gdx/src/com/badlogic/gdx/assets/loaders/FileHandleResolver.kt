4d51d956289e61956c346f2199dbe0785516b06a
hojat72elect <hojat72elect@gmail.com>
2026-01-03 17:02:33 -0500

    Moved FileHandleResolver.kt to kerman game engine.

--- gdx/src/com/badlogic/gdx/assets/loaders/FileHandleResolver.kt
+++ gdx/src/com/badlogic/gdx/assets/loaders/FileHandleResolver.kt
@@ -4,6 +4,8 @@ import com.badlogic.gdx.assets.AssetManager
 import com.badlogic.gdx.files.FileHandle
 
 /**
+ * Info : This interface was moved to Kerman game engine.
+ *
  * Interface for classes which can map a file name to a [FileHandle]. Used to allow the [AssetManager] to load
  * resources from anywhere or implement caching strategies.
  */
