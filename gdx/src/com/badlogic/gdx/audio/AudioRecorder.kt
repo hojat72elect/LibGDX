72dd2a25fc83b8af903bb552d74453255160165b
hojat72elect <hojat72elect@gmail.com>
2026-01-03 19:38:07 -0500

    Moved AudioRecorder.kt to kerman game engine.

--- gdx/src/com/badlogic/gdx/audio/AudioRecorder.kt
+++ gdx/src/com/badlogic/gdx/audio/AudioRecorder.kt
@@ -3,6 +3,8 @@ package com.badlogic.gdx.audio
 import com.badlogic.gdx.utils.Disposable
 
 /**
+ * Info : This interface was moved to Kerman game engine.
+ *
  * An AudioRecorder allows to record input from an audio device. It has a sampling rate and is either stereo or mono. Samples are
  * returned in signed 16-bit PCM format. Stereo samples are interleaved in the order left channel, right channel. The
  * AudioRecorder has to be disposed if no longer needed via the [.dispose].
@@ -13,8 +15,8 @@ interface AudioRecorder : Disposable {
      * Reads in [numSamples] samples into the array [samples] starting at [offset]. If the recorder is in stereo you have to multiply
      * [numSamples] by 2.
      *
-     * @param samples    the array to write the samples to
-     * @param offset     the offset into the array
+     * @param samples the array to write the samples to
+     * @param offset the offset into the array
      * @param numSamples the number of samples to be read
      */
     fun read(samples: ShortArray, offset: Int, numSamples: Int)
