1f66dac950cb1f86ebde48aa4e292bb0066b0db3
hojat72elect <hojat72elect@gmail.com>
2026-01-04 14:05:26 -0500

    Moved ModelNodeKeyframe.java to kerman game engine.

--- gdx/src/com/badlogic/gdx/graphics/g3d/model/data/ModelNodeKeyframe.java
+++ gdx/src/com/badlogic/gdx/graphics/g3d/model/data/ModelNodeKeyframe.java
@@ -1,5 +1,8 @@
 package com.badlogic.gdx.graphics.g3d.model.data;
 
+/**
+ * This class was moved to Kerman game engine.
+ */
 public class ModelNodeKeyframe<T> {
     /**
      * the timestamp of the keyframe in seconds
