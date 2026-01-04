84bde74284d7960a19aeae3016becc57221a274d
hojat72elect <hojat72elect@gmail.com>
2026-01-04 00:19:04 -0500

    Moved ModelMeshPart.java to kerman game engine.

--- gdx/src/com/badlogic/gdx/graphics/g3d/model/data/ModelMeshPart.java
+++ gdx/src/com/badlogic/gdx/graphics/g3d/model/data/ModelMeshPart.java
@@ -1,5 +1,8 @@
 package com.badlogic.gdx.graphics.g3d.model.data;
 
+/**
+ * Info : This class was moved to Kerman game engine.
+ */
 public class ModelMeshPart {
     public String id;
     public short[] indices;
