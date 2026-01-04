de8bfecf629aa98cb457634bcb97aa57118c4b51
hojat72elect <hojat72elect@gmail.com>
2026-01-03 22:34:37 -0500

    Moved ModelAnimation.java to kerman game engine.

--- gdx/src/com/badlogic/gdx/graphics/g3d/model/data/ModelAnimation.java
+++ gdx/src/com/badlogic/gdx/graphics/g3d/model/data/ModelAnimation.java
@@ -2,7 +2,10 @@ package com.badlogic.gdx.graphics.g3d.model.data;
 
 import com.badlogic.gdx.utils.Array;
 
+/**
+ * Info : This class was moved to Kerman game engine.
+ */
 public class ModelAnimation {
     public String id;
-    public Array<ModelNodeAnimation> nodeAnimations = new Array<ModelNodeAnimation>();
+    public Array<ModelNodeAnimation> nodeAnimations = new Array<>();
 }
