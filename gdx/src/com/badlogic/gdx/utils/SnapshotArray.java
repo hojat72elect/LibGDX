125264c458d275a6a784873f97419844954e3075
hojat72elect <hojat72elect@gmail.com>
2026-01-08 00:56:42 -0500

    Moved SnapshotArray.java to kerman game engine.

--- gdx/src/com/badlogic/gdx/utils/SnapshotArray.java
+++ gdx/src/com/badlogic/gdx/utils/SnapshotArray.java
@@ -4,6 +4,8 @@ import java.util.Arrays;
 import java.util.Comparator;
 
 /**
+ * Info : This class was moved to Kerman game engine.
+ * <p>
  * An array that allows modification during iteration. Guarantees that array entries provided by {@link #begin()} between indexes
  * 0 and {@link #size} at the time begin was called will not be modified until {@link #end()} is called. If modification of the
  * SnapshotArray occurs between begin/end, the backing array is copied prior to the modification, ensuring that the backing array
