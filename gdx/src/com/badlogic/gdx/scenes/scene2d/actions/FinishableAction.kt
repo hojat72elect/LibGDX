5a26bbe317928b6f95be8f076f2b5ddad22ce7cd
hojat72elect <hojat72elect@gmail.com>
2026-01-09 21:21:04 -0500

    Moved FinishableAction.kt to kerman game engine.

--- gdx/src/com/badlogic/gdx/scenes/scene2d/actions/FinishableAction.kt
+++ gdx/src/com/badlogic/gdx/scenes/scene2d/actions/FinishableAction.kt
@@ -1,6 +1,8 @@
 package com.badlogic.gdx.scenes.scene2d.actions
 
 /**
+ * Info : This interface was moved to Kerman game engine.
+ *
  * An interface for actions that can be finished manually.
  */
 interface FinishableAction {
