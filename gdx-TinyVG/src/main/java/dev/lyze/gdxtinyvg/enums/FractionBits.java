7adb5e5570c28b33dacc87c5b48016c32c179ebe
hojat72elect <hojat72elect@gmail.com>
2026-02-01 01:08:32 -0500

    Removed the `gdx-TinyVG` module.
    #22

--- gdx-TinyVG/src/main/java/dev/lyze/gdxtinyvg/enums/FractionBits.java
+++
@@ -1,7 +0,0 @@
-package dev.lyze.gdxtinyvg.enums;
-
-public class FractionBits {
-    public static float calculate(int value, int fractionBits) {
-        return value / (float) Math.pow(2, fractionBits);
-    }
-}
